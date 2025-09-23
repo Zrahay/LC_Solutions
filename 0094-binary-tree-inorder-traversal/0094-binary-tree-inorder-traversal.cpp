4d31d0ab57275f094bdd8e859e256af31fd6436d
Shubhang Sinha <145453984+Zrahay@users.noreply.github.com>
2025-09-23 19:51:58 +0530

    Time: 0 ms (100%), Space: 11 MB (32.01%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -6,23 +6,47 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
-    vector<int>ans;
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        if(root == NULL) return ans;
+        vector<int> v;
+        if (root == NULL)
+            return v;
+
+        stack<TreeNode*> st;
+        TreeNode* node = root;
 
         
 
-        inorderTraversal(root->left);
+        // The initial idea is to push all the left nodes into the left side
 
-        ans.push_back(root->val);
+        while (true) {
 
-        inorderTraversal(root->right);
+            while(node != NULL) {
+                st.push(node);
+                node = node->left;
+            }
 
-        return ans;
+            if(st.empty() == true) break;
+
+            if(node == NULL) {
+                node = st.top();
+                st.pop();
+
+                v.push_back(node->val);
+                
+
+                node = node->right;
+            }
+        }
+
+        return v;
+
+        // Now we have all the left nodes in tact
+        // Should I go on and check the right side?
     }
 };
